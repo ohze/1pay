cb8f2e3b0a33c6e4ad29b0964ebacf4aefcdf7fd
giabao <giabao@sandinh.net>
2015-12-22 21:26:56 +0700

    we must use parse.anyContent or the body of parsed request will be empty

--- example/app/controllers/Sms.scala
+++ example/app/controllers/Sms.scala
@@ -9,11 +9,11 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
 @Singleton
 class Sms @Inject() (sms1Pay: SMS) extends Controller {
 
-  def check() = Action.async { implicit req =>
+  def check() = Action.async(parse.anyContent) { implicit req =>
     sms1Pay.check.map(Ok(_))
   }
 
-  def charge() = Action.async { implicit req =>
+  def charge() = Action.async(parse.anyContent) { implicit req =>
     sms1Pay.charge.map(Ok(_))
   }
 }
